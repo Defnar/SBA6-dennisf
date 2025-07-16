d08ef58a50ca4730f909a2e1ce74dc7fc2d2683f
Dennis <dennis-farris@hotmail.com>
2025-07-16 17:03:51 -0400

    Fixing typescript in index.ts to match proper output on getProduct

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ import calculateTax from "./src/utils/taxCalculator";
 
 const url = 'https://dummyjson.com/products/1';
 
-async function getProduct(url: string) {
+async function getProduct(url: string): Promise<Product> {
     const productData = await fetchData(url);
     const product = new Product(productData.title, productData.category, productData.price, productData.discountPercentage)
     return product;
